ba320ac519abbc80826af34815854be72e8e923b
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-08-16 14:32:21 +0200

    inicio de home update

--- frontend/src/Pages/PublicPage/HomePage.tsx
+++ frontend/src/Pages/PublicPage/HomePage.tsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from 'react';
+import './styles/HomePage.css';
 import Footer from '../../components/Public/Footer'
 import Header from '../../components/Public/Header'
 import Summary from '../../components/Public/Summary'
 
 function HomePage() {
+  const [showPage, setShowPage] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowPage(true);
+    }, 30); 
+  }, []);
+
   return (
-    <>
-      <Header />
-      <Summary/>
-      <Footer />
-    </>
-  )
+    <div className="home">
+      <div className={`home-page ${showPage ? 'active' : ''}`}>
+        <Header />
+        <Summary />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
